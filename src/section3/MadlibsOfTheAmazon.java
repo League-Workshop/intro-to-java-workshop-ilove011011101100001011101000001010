6298e3b2d3c61d2e893db4551801ebd8b3a8d878
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-02-14 13:02:59 -0800

    fish the madlibs

--- src/section3/MadlibsOfTheAmazon.java
+++ src/section3/MadlibsOfTheAmazon.java
@@ -7,9 +7,9 @@ public class MadlibsOfTheAmazon {
 	public static void main(String[] args) {
 		// Put this sentence in a pop up:
 		// If you find yourself having to cross a piranha-infested river, here's how to do it...
-		JOptionPane.ShowInputMessageDialog(" a piranha-infested river, here's how to do it...");		
+		JOptionPane.showInputDialog(" a piranha-infested river, here's how to do it...");		
 		// Get the user to enter an adjective
-		String answer= JOptionPane.showInputDialog("adjective");
+		String adjective= JOptionPane.showInputDialog("adjective");
 		// Get the user to enter a type of liquid
 String liquid=JOptionPane.showInputDialog("liquid");
 		// Get the user to enter a body part
@@ -20,14 +20,14 @@ String verd=JOptionPane.showInputDialog("verb");
 String place=JOptionPane.showInputDialog("place");
 		// Fit the user's words into this sentence, and save it in a String:
 		// Piranhas are more [adjective] 
-String sentence= "Piranhas are more"+adjective+"during the day, so cross the river at"+
+String sentence= "Piranhas are- more "+adjective+" during the day,\n so cross the river at\n"+
 		
-" night. Piranhas are attracted to fresh "+ liquid +" and will most"+
-" likely take a bite out of your "+bodypart if you [verb]. Whatever
-// you do, if you have an open wound, try to find another way to get
-// back to the [place]. Good luck!
+" night. Piranhas are attracted to fresh "+ liquid +" and will most\n"+
+" likely take a bite out of your "+bodypart+" if you "+verd+". Whatever\n"+
+"you do, if you have an open wound, try to find another way to get\n"+
+"+back to the "+place+". Good luck!";
 		// Make a pop-up for the final story. You can use \n to go to the next line
-		
+		JOptionPane.showMessageDialog(null, sentence);
 
 	}
 }
